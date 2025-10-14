9b15c8e5deb7abaf4debdbd7631b168697becc5b
Ayako Nakayama <j9griffon0220@gmail.com>
2025-10-14 20:35:14 +0900

    Postモデルを作成（User・Threadとのリレーションを定義）

---
+++ app/Models/Post.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    /** @use HasFactory<\Database\Factories\PostFactory> */
+    use HasFactory;
+
+    // 代入を許可するカラム
+    protected $fillable = [
+        'title',
+        'body',
+        'user_id',
+        'thread_id',
+        'is_edited',
+    ];
+
+    // リレーション（他のモデルとのつながり）
+    // ポスト（投稿）には1人の作成したユーザーがいる
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    // ポスト（投稿）には所属するThreadが1つある
+    public function thread(){
+        return $this->belongTo(Thread::class);
+    }
+}

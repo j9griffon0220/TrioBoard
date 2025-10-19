42ab122052004d679debb1449141b1021291309f
Ayako Nakayama <j9griffon0220@gmail.com>
2025-10-19 13:07:08 +0900

    SeederでUserと関連するスレッド・投稿を生成

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory()
+        ->count(3)
+        ->hasThreads(3)
+        ->hasPosts(2)
+        ->create();
+
+
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }

9ce5dcbd6f2ce35bfeb69fb8663629618031af87
Ayako Nakayama <j9griffon0220@gmail.com>
2025-10-19 16:27:21 +0900

    掲示板全体のベースレイアウトを作成（調整これから）

---
+++ resources/views/layouts/board_base.blade.php
@@ -0,0 +1,20 @@
+{{-- 掲示板全体のベースレイアウト --}}
+<!DOCTYPE html>
+<html lang="ja">
+    {{-- headタグ --}}
+    @include('layouts.board_head')
+
+<body>
+    <div>
+    {{-- ヘッダー --}}
+    @include('layouts.board_header')
+
+    {{-- コンテンツ --}}
+    @yield('board_content')
+
+    {{-- フッター --}}
+    @include('layouts.board_footer')
+    </div>
+
+</body>
+</html>

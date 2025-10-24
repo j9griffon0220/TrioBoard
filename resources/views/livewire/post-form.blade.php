650a424720cb9517aef6d8b544b222f11c8edab0
Ayako Nakayama <j9griffon0220@gmail.com>
2025-10-24 21:10:00 +0900

    artisanコマンドでlivewireコンポーネント、postのviewファイル記述

--- resources/views/livewire/post-form.blade.php
+++ resources/views/livewire/post-form.blade.php
@@ -1,4 +1,17 @@
 <div>
     {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
-    <h1>これはlivewireコンポーネントです</h1>
+    <h1>投稿フォーム</h1>
+
+    <input type="text" wire:model="title" placeholder="タイトル">
+    <textarea wire:model="body" placeholder="投稿本文"></textarea>
+
+    @if ($isEditing)
+        <form wire:submit.prevent="destroy">
+            <button type="submit">投稿を破棄</button>
+        </form>
+    @else
+        <form wire:submit.prevent="store">
+            <button type="submit">投稿を保存</button>
+        </form>
+    @endif
 </div>

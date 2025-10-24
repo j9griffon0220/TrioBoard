b8dc6200a6338442b3265dc1d199ef951dc2be36
Ayako Nakayama <j9griffon0220@gmail.com>
2025-10-24 21:06:29 +0900

    livewireコンポーネントファイルを生成、post（form）バリデーションを記述

--- app/Livewire/PostForm.php
+++ app/Livewire/PostForm.php
@@ -7,11 +7,41 @@ use App\Models\Post;
 
 class PostForm extends Component
 {
-    public $title;
-    public $body;
+    public $title = '';
+    public $body = '';
+    public $isEditing = false;
 
-    public function render()
+    // コンポーネント単位でバリデーション
+    protected $rules = [
+        'title' => 'required|string|max:40',
+        'body' => 'required|string|max:400',
+    ];
+
+    public function store()
     {
-        return view('livewire.post-form');
+        $this->validate();
+
+        Post::create([
+            'title' => $this->title,
+            'body' => $this->body,
+            'user_id' => auth()->id(),
+        ]);
+
+        //フォームを初期化
+        $this->reset();
+    }
+
+    // 投稿者のみが投稿を破棄可能にする
+    public function destroy(){
+        if(auth()->id() !== $this->post->user_id){
+            abort(403, '許可されていません。投稿者本人のみ可能です');
+        }
+        $this->post->delete();
+        session()->flash('success', '投稿を破棄しました');
     }
 }
+
+    // public function render()
+    // {
+    //     return view('livewire.post-form');
+    // }

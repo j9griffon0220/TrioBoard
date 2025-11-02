15b968d8b62b48d08b863ad055fd1a01b4b29cef
Ayako Nakayama <j9griffon0220@gmail.com>
2025-11-02 14:19:32 +0900

    スレッド一覧のindex.blade、表示のための記述を作成

--- resources/views/Threads/index.blade.php
+++ resources/views/Threads/index.blade.php
@@ -1,7 +1,20 @@
 @extends('layouts.board_base')
 @section('board_content')
-<h1>スレッド一覧</h1>
-{{-- スレッド表示 --}}
 
+<h1>スレッド一覧</h1>
 <a href="{{ route('threads.create') }}">スレッド新規作成</a>
+
+{{-- スレッド表示 --}}
+<ul>
+    @forelse ($threads as $thread)
+        <li>
+            <a href="{{ route('threads.show', $thread->id )}}">
+                {{$thread->title}}
+            </a>
+        </li>
+    @empty
+    <p>まだスレッドがありません</p>
+    @endforelse
+</ul>
+
 @endsection

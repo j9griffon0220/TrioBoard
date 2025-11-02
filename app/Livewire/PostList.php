48e5011669fc0021833ede97de1f0672fafd8694
Ayako Nakayama <j9griffon0220@gmail.com>
2025-11-02 14:18:00 +0900

    投稿一覧のPostList、処理を記述中

--- app/Livewire/PostList.php
+++ app/Livewire/PostList.php
@@ -3,12 +3,34 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Thread;
 use App\Models\Post;
+use App\Models\User;
+use App\Enums\Role;
 
 class PostList extends Component
 {
+    public $posts = [];
+
+    // blade から渡されるThread を受け取る
+    public Thread $selectedthread;
+
+    // public function mount($selectedthread)
+    // {
+    //     $this->selectedthread = $selectedthread;
+    // }
+
     public function render()
     {
-        return view('livewire.post-list');
+        $posts = $this->selectedthread->posts()->latest()->get();
+        // デバッグ
+        // dd($posts);
+        // dd($this->selectedthread->posts()->count());
+        return view('livewire.post-list', compact('posts'));
     }
+
+
+    // 子のpost-formから送られたイベントを受け取る
+    protected $listeners = ['postAdded' => 'refresh'];
+
 }

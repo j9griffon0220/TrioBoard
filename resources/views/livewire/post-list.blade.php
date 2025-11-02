adc0efffef137bbdbed4046d06e2e84b90367f8e
Ayako Nakayama <j9griffon0220@gmail.com>
2025-11-02 13:57:55 +0900

    投稿一覧を表示するためのpost-list.bladeを作成、編集中

--- resources/views/livewire/post-list.blade.php
+++ resources/views/livewire/post-list.blade.php
@@ -1,4 +1,19 @@
 <div>
     {{-- The best athlete wants his opponent at his best. --}}
     <h1>これはlivewireのポスト一覧です</h1>
+    @forelse($posts as $post)
+    {{-- postごとにまとめる --}}
+    <div>
+        {{-- <p>{{ $post->user->name }}</p> --}}
+        {{-- null 安全に表示する --}}
+        <p>{{ optional($post->user)->name ?? '(投稿者不明)'}}</p>
+        <p>{{ $post->title }}</p>
+        <p>{{ $post->body }}</p>
+    </div>
+    @empty
+    <p>まだ投稿がありません</p>
+    @endforelse
+
+    {{-- 投稿用のlivewireコンポーネント --}}
+    <livewire:post-form :thread-id="$thread->id" />
 </div>

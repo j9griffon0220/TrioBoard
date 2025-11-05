1e8bcbd60ba5df9baf3901b3f8c25768dd0385f9
Ayako Nakayama <j9griffon0220@gmail.com>
2025-11-05 21:43:09 +0900

    PostList.phpでLivewireコンポーネントをリアクティブ処理できるよう調整、Onの受け取り追加

--- app/Livewire/PostList.php
+++ app/Livewire/PostList.php
@@ -7,30 +7,35 @@ use App\Models\Thread;
 use App\Models\Post;
 use App\Models\User;
 use App\Enums\Role;
+use Livewire\Attributes\On;
 
 class PostList extends Component
 {
     public $posts = [];
 
     // blade から渡されるThread を受け取る
-    public Thread $selectedthread;
+    public Thread $thread;
 
-    // public function mount($selectedthread)
-    // {
-    //     $this->selectedthread = $selectedthread;
-    // }
+    public function mount(Thread $thread)
+    {
+        $this->thread = $thread;
+    }
 
     public function render()
     {
-        $posts = $this->selectedthread->posts()->latest()->get();
+        $this->posts = $this->thread->posts()->latest()->get();
         // デバッグ
         // dd($posts);
         // dd($this->selectedthread->posts()->count());
-        return view('livewire.post-list', compact('posts'));
+        return view('livewire.post-list');
     }
 
-
     // 子のpost-formから送られたイベントを受け取る
-    protected $listeners = ['postAdded' => 'refresh'];
+    #[On('postAdded')]
+    public function refresh($post)
+    {
+        $this->loadPosts();
+    }
+    // protected $listeners = ['postAdded' => 'refresh'];
 
 }

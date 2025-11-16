04ce90d020726ea232f351947c97796b1e9af3a1
Ayako Nakayama <j9griffon0220@gmail.com>
2025-11-16 12:17:46 +0900

    TestCase.phpに全てのテストに共通のクラス（roleごとの設定）を作成

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,38 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use App\Models\User;
+use App\Enums\Role;
+
+// abstract class TestCase extends BaseTestCase
+// {
+//     //
+// }
+
+// tests/TestCase.php はすべての Feature / Unit テストの親クラス
+// このファイルに書けば、すべてのテストで使える
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    // テスト用にAdminを共通化
+    protected function createAdmin()
+    {
+        return User::factory()->create([
+            'role' => Role::Admin,
+        ]);
+    }
+
+    // Memberはデフォルトだがテスト用に明示
+    protected function createMember(){
+        return User::factory()->create([
+            'role' => Role::Member,
+        ]);
+    }
+
+    // Viewer（閲覧のみの方）
+    protected function createViewer(){
+        return User::factory()->create([
+            'role' => Role::Viewer,
+        ]);
+    }
 }

26b90079551c589a62dae2d311a661aeeb9cc037
Ayako Nakayama <j9griffon0220@gmail.com>
2025-11-22 15:20:11 +0900

    Postの投稿処理にPolicy適用処理を追加

--- app/Livewire/PostForm.php
+++ app/Livewire/PostForm.php
@@ -5,9 +5,13 @@ namespace App\Livewire;
 use Livewire\Component;
 use App\Models\Thread;
 use App\Models\Post;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class PostForm extends Component
 {
+    // これで authorize() が使えるようになる
+    use AuthorizesRequests;
+
     // public Thread $selectedthread;
     public Thread $thread;
 
@@ -27,7 +31,7 @@ class PostForm extends Component
         return view('livewire.post-form');
     }
 
-    // コンポーネント単位でバリデーション
+    // コンポーネント単位のバリデーション設定
     protected $rules = [
         'title' => 'required|string|max:40',
         'body' => 'required|string|max:400',
@@ -35,10 +39,14 @@ class PostForm extends Component
 
     public function store()
     {
+        // Policyで権限チェック（Threadを渡してコンテキストを与える）
+        // 第2引数に配列で渡すと、最初の要素でポリシーが決定、残りが引数として渡される
+        $this->authorize('create', Post::class);
+
         // バリデーションを実行
         $this->validate();
 
-        // 選択中スレッドに投稿を紐付ける（リレーション経由で子モデルを作成）
+        // Postの投稿作成、選択中スレッドに投稿を紐付ける（リレーション経由で子モデルを作成）
         $post = $this->thread->posts()->create([
             'title' => $this->title,
             'body' => $this->body,
@@ -48,7 +56,7 @@ class PostForm extends Component
         // 投稿イベントを親に知らせる
         $this->dispatch('postAdded', post: $post);
 
-        //フォームを初期化
+        // フォームを初期化
         $this->reset(['title','body']);
     }
 }

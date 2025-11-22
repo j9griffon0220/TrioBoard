bb79a24b1b4e1e2b97d995e9fb25b1bee29a90df
Ayako Nakayama <j9griffon0220@gmail.com>
2025-11-22 15:12:35 +0900

    seederにテスト用memberを1名追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,23 +17,6 @@ class DatabaseSeeder extends Seeder
         ->count(3)
         ->hasThreads(3)
         ->hasPosts(2)
-        ->create();
 
-        // viewerユーザーを１人作成
-        User::factory()->create([
-            'name' => 'viewer',
-            'email' => 'viewer@example.com',
-            'role' => Role::Viewer,
-            'password' => bcrypt('viewer'),
-            'two_factor_secret' => null,
-            'two_factor_recovery_codes' => null,
-        ]);
-
-        // User::factory(10)->create();
-
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }

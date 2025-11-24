c958a1a98b05f518d6e3f8a263e94a8b70bbeb72
Ayako Nakayama <j9griffon0220@gmail.com>
2025-11-24 17:53:19 +0900

    Postページのroleごとのアクセス制限、投稿フォーム表示・非表示のテスト〜pass

--- tests/Feature/PostAccessTest.php
+++ tests/Feature/PostAccessTest.php
@@ -1,4 +1,8 @@
 <?php
+use App\Models\User;
+use App\Models\Thread;
+use App\Models\Post;
+use App\Enums\Role;
 
 // test('example', function () {
 //     $response = $this->get('/');
@@ -6,9 +10,42 @@
 //     $response->assertStatus(200);
 // });
 
-it('未ログインの方は新規投稿画面にアクセスできない', function(){
-        // テスト用スレッドデータを生成
+// Post（投稿）ページのテスト
+it('admin、member、viewerはポスト一覧ページにアクセス可能', function($role, $expected){
+    $user = User::factory()->create(['role' => $role]);
+
+    // テスト用データを生成
     $thread = Thread::factory()->create();
-    $response = $this->get("/threads/{$thread->id}");
-    $response = $this->get();
+    $response = $this->actingAs($user)->get(route('threads.show', $thread->id));
+    $response->assertStatus($expected);
+})->with([
+    [Role::Admin, 200],
+    [Role::Member, 200],
+    [Role::Viewer, 200],
+]);
+
+it('非登録者は投稿ページにアクセスできない', function(){
+    // テスト用スレッドデータを生成
+    $thread = Thread::factory()->create();
+    $response = $this->get(route('threads.show', $thread->id));
+    $response ->assertRedirect('/login');
+});
+
+// 投稿フォームの表示のテスト
+it('権限のあるadmin、memberは投稿フォームが表示される', function($role){
+    $user = User::factory()->create(['role' => $role]);
+    $thread = Thread::factory()->create();
+
+    $response = $this->actingAs($user)->get(route('threads.show', $thread->id));
+    $response->assertSeeLivewire('post-form');
+})->with([
+    Role::Admin,
+    Role::Member,
+]);
+
+it('viewerには投稿フォームが表示されない', function(){
+    $thread = Thread::factory()->create();
+    $response = $this->get(route('threads.show', $thread->id));
+    // Livewireコンポーネントが出てこないことを確認
+    $response->assertDontSee('livewire:post-form');
 });

b234e998e21724ee09d82a4e5ce332e219a37fa4
Ayako Nakayama <j9griffon0220@gmail.com>
2025-12-07 15:05:32 +0900

    デフォルトlogin画面のfaviconとtitleをTrio Board用に変更

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,11 +1,18 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-<title>{{ $title ?? config('app.name') }}</title>
+{{-- <title>{{ $title ?? config('app.name') }}</title> --}}
+<title>Trio Board</title>
 
-<link rel="icon" href="/favicon.ico" sizes="any">
-<link rel="icon" href="/favicon.svg" type="image/svg+xml">
-<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+{{-- <link rel="icon" href="/favicon.ico" sizes="any"> --}}
+{{-- <link rel="icon" href="/favicon.svg" type="image/svg+xml"> --}}
+{{-- <link rel="apple-touch-icon" href="/apple-touch-icon.png"> --}}
+<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+<link rel="shortcut icon" href="/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+<meta name="apple-mobile-web-app-title" content="TrioBoard" />
+<link rel="manifest" href="/site.webmanifest" />
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />

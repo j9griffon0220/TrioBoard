7d83fa313f24b539d16d9f87d3ad5cc61c75d091
Ayako Nakayama <j9griffon0220@gmail.com>
2025-12-07 15:01:59 +0900

    トップページにメインのロゴアイコン画像を配置

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,11 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>掲示板トップページ</title>
+    <title>Trio Board</title>
     @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 <body>
     <div>
+        <picture>
+            <source media="" srcset="{{ asset('images/trio-board-logo@1x.webp') }}" type="image/webp">
+            <source srcset="{{ asset('images/trio-board-logo@2x.webp') }} 2x" type="image/webp">
+            <img src="{{ asset('trio-board-logo.png')}}" alt="TrioBoardLogo">
+        </picture>
         <h1>ここがindex.blade.phpです</h1>
         <a href="{{ route('login')}}" class="underline">ログインページへ</a>
     </div>

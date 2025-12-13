4a5f4be6a7cee0fb7a2b51efb4638dab5793bb0e
Ayako Nakayama <j9griffon0220@gmail.com>
2025-12-13 14:47:56 +0900

    bladeのbaseのdead、title部分微調整

--- resources/views/layouts/board_head.blade.php
+++ resources/views/layouts/board_head.blade.php
@@ -8,7 +8,7 @@
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
     <meta name="apple-mobile-web-app-title" content="TrioBoard" />
     <link rel="manifest" href="/site.webmanifest" />
-    <title>TrioBoard</title>
+    <title>Trio Board</title>
     @livewireStyles
     @vite('resources/css/app.css')
 </head>

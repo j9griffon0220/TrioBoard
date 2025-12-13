79b0b9fd40933b53a9c570e82bc463bd99a4a9c4
Ayako Nakayama <j9griffon0220@gmail.com>
2025-12-13 14:51:09 +0900

    bladeのbaseテンプレートに開発時のみデバッグ用outlineを出す設定追加

--- resources/views/layouts/board_base.blade.php
+++ resources/views/layouts/board_base.blade.php
@@ -4,16 +4,21 @@
     {{-- headタグ --}}
     @include('layouts.board_head')
 
-<body>
-    <div>
-    {{-- ヘッダー --}}
-    @include('layouts.board_header')
+{{-- 開発環境だけでデバッグ用クラスを出すようbodyタグで調整 --}}
+<body @class(['debug-outline' => app()->environment('local')])>
+    <div class="bg-board-grey min-h-screen">
+        <div class="max-w-[90vw] mx-auto">
+        {{-- ヘッダー --}}
+        @include('layouts.board_header')
 
-    {{-- コンテンツ --}}
-    @yield('board_content')
+        <div class="mt-[var(--s3)]">
+        {{-- コンテンツ --}}
+        @yield('board_content')
+        </div>
 
-    {{-- フッター --}}
-    @include('layouts.board_footer')
+        {{-- フッター --}}
+        @include('layouts.board_footer')
+        </div>
     </div>
     @livewireScripts
 </body>

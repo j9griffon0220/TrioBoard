006d636a3dba9886f3abf1d3c855f52e3ea57b5e
Ayako Nakayama <j9griffon0220@gmail.com>
2025-12-14 16:28:46 +0900

    npx prettier --write resources/views　における調整

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,29 +1,38 @@
 @extends('layouts.board_base')
 
 @section('board_content')
-<div class="max-w-[25vw] mx-auto mt-[var(--s4)]">
+  <div class="mx-auto mt-[var(--s4)] max-w-[25vw]">
     <picture>
-        <source
-        srcset="{{ asset('images/error404@1x.webp') }} 1x,
-        {{ asset('images/error404@2x.webp') }} 2x"
-        type="image/webp">
-        {{-- フォールバック --}}
-        <img src="{{ asset('images/error404.png') }}" alt="Trio Board Logo" class="w-full h-auto">
+      <source
+        srcset="
+          {{ asset('images/error404@1x.webp') }} 1x,
+          {{ asset('images/error404@2x.webp') }} 2x
+        "
+        type="image/webp"
+      />
+      {{-- フォールバック --}}
+      <img src="{{ asset('images/error404.png') }}" alt="Trio Board Logo" class="h-auto w-full" />
     </picture>
-</div>
+  </div>
 
-<h1 class="text-double-blue font-title text-center mt-[var(--s4)]">404 – ページが見つかりません</h1>
-<p class="text-board-black font-body text-center mt-[var(--s4)]">
+  <h1 class="mt-[var(--s4)] text-center font-title text-double-blue">
+    404 – ページが見つかりません
+  </h1>
+  <p class="mt-[var(--s4)] text-center font-body text-board-black">
     URLが間違っているか、ページが移動した可能性があります。
-    <br>お手数ですが、トップページから再度お探しください。</p>
+    <br />
+    お手数ですが、トップページから再度お探しください。
+  </p>
 
-    <div class="text-center">
-        <a href="{{ route('login') }}" class="underline">ログインページへ</a>
-    </div>
+  <div class="text-center">
+    <a href="{{ route('login') }}" class="underline">ログインページへ</a>
+  </div>
 @endsection
 
-{{-- @extends('errors::minimal')
-
-@section('title', __('Not Found'))
-@section('code', '404')
-@section('message', __('Not Found')) --}}
+{{--
+  @extends('errors::minimal')
+  
+  @section('title', __('Not Found'))
+  @section('code', '404')
+  @section('message', __('Not Found'))
+--}}

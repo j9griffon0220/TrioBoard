6e1b15ece4a1382d44fa9d11ca4fedf1e786717f
Ayako Nakayama <j9griffon0220@gmail.com>
2025-12-14 16:23:52 +0900

    npx prettier --write resources/views　における調整

--- resources/views/Threads/show.blade.php
+++ resources/views/Threads/show.blade.php
@@ -2,18 +2,16 @@
 
 {{-- livewireのpost-listをコンポーネントとして使う --}}
 @section('board_content')
+  {{-- livewireコンポーネントに $threadを渡す --}}
+  <livewire:post-list :thread="$thread" />
 
-{{-- livewireコンポーネントに $threadを渡す --}}
-<livewire:post-list :thread="$thread" />
+  {{-- 投稿用のlivewireコンポーネント --}}
+  @can('create', \App\Models\Post::class)
+    <livewire:post-form :thread="$thread" />
+  @endcan
 
-{{-- 投稿用のlivewireコンポーネント --}}
-@can('create',\App\Models\Post::class)
-<livewire:post-form :thread="$thread" />
-@endcan
-
-{{-- スレッド一覧に戻るボタン --}}
-<button>
+  {{-- スレッド一覧に戻るボタン --}}
+  <button>
     <a href="{{ route('threads.index') }}">スレッド一覧に戻る</a>
-</button>
-
+  </button>
 @endsection

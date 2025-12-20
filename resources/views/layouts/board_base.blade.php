c1c5a2957ad5c5ad29b601a3ac7b63ba7f814d5e
Ayako Nakayama <j9griffon0220@gmail.com>
2025-12-20 17:43:11 +0900

    bladeのbaseファイル、レイアウト調整（pt、pbでブラウザの上下余白を追加）

--- resources/views/layouts/board_base.blade.php
+++ resources/views/layouts/board_base.blade.php
@@ -7,17 +7,17 @@
   {{-- 開発環境だけでデバッグ用クラスを出すようbodyタグで調整 --}}
   <body @class(['debug-outline' => app()->environment('local')])>
     <div class="min-h-screen bg-board-grey">
-      <div class="mx-auto max-w-[90vw]">
+      <div class="mx-auto max-w-[90vw] pt-[var(--s3)] pb-[var(--s3)]">
+
         {{-- ヘッダー --}}
         @include('layouts.board_header')
 
-        <div class="mt-[var(--s3)]">
-          {{-- コンテンツ --}}
-          @yield('board_content')
-        </div>
+        {{-- コンテンツ --}}
+        @yield('board_content')
 
         {{-- フッター --}}
         @include('layouts.board_footer')
+
       </div>
     </div>
     @livewireScripts

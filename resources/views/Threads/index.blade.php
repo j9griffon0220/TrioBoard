dc03374d4fe853ef2edd0e8590099f0aef7846b0
Ayako Nakayama <j9griffon0220@gmail.com>
2025-12-20 17:39:25 +0900

    threadsの一覧画面、mbからmt調整に変更、logoutをpngに変更、cssのスタイリング調整

--- resources/views/Threads/index.blade.php
+++ resources/views/Threads/index.blade.php
@@ -1,50 +1,47 @@
 @extends('layouts.board_base')
 @section('board_content')
 
-<h1 class="text-double-blue font-title text-center mt-[var(--s4)]">スレッド一覧</h1>
+<h1 class="text-double-blue font-title text-center mb-[var(--s4)] font-medium text-xl">スレッド一覧</h1>
 @can('create',\App\Models\Thread::class)
 <div class="flex justify-end">
-    <div class="max-w-[20vw] border-2 p-[var(--s-3)] rounded-2xl border-double-blue text-center">
+    <div class="min-w-[15ch] text-center mb-[var(--s4)]">
     <a href="{{ route('threads.create') }}"
-    class="block text-double-blue font-title text-center">スレッド新規作成</a>
+    class="block font-title text-center text-base btn btn-primary">スレッド作成</a>
     </div>
 </div>
 @endcan
 
 @if(session('success'))
     <div>
-        {{ session('success') }}
+        <p class="text-double-red font-body font-normal">{{ session('success') }}</p>
     </div>
 @endif
 
 {{-- スレッド表示 --}}
-<ul>
+<ul class="">
     @forelse ($threads as $thread)
-        <li class="text-board-black font-body border-2 p-[var(--s-4)] border-board-blue mt-[var(--s1)] max-w-[80vw]">
+        <li class="text-board-black font-body font-normal text-base border-1 p-[var(--s-3)] border-board-border mb-[var(--s4)] max-w-[80vw] border-l-5 border-l-board-blue
+        hover:shadow-xl transition-shadow bg-gray-50">
             <a href="{{ route('threads.show', $thread->id )}}">
                 {{ $thread->title }}
-            <span class="font-body text-board-charcoal" title="{{ $thread->created_at->format('Y-m-d H:i')}}">
-                {{ $thread->created_at->diffForHumans() }}
+            <span class="font-body text-board-charcoal font-light text-sm" title="{{ $thread->created_at->format('Y-m-d H:i')}}">
+                （{{ $thread->created_at->diffForHumans() }}）
             </span>
             </a>
         </li>
     @empty
-    <p class="text-board-black font-body">まだスレッドがありません</p>
+    <p class="text-board-black font-body font-normal text-base">まだスレッドがありません</p>
     @endforelse
 </ul>
 
-<div class="flex justify-end">
-    <div class="max-w-[20vw] mt-[var(--s4)] block border-2 p-[var(--s-3)] rounded-2xl border-double-blue shadow-md block">
+<div class="flex justify-end mb-[var(--s4)]">
+    <div class="max-w-[15ch]">
         <form method="POST" action="{{ route('logout') }}">
         @csrf
-        <button type="submit">
-            <picture>
-            <source
-            srcset="{{ asset('images/board-logout@1x.webp') }} 1x,{{ asset('images/board-logout@2x.webp') }} 2x"
-            type="image/webp">
-            {{-- フォールバック --}}
-            <img src="{{ asset('images/board-logout.png') }}" alt="Logout" class="w-full h-auto">
-            </picture>
+        <button type="submit" class="btn btn-outline">
+          <img src="image.png"
+          srcset="{{ asset('images/board-logout@1x.png') }} 1x,
+          {{ asset('images/board-logout@2x.png')}} 2x" alt="Login" class="h-auto w-full">
         </button>
         </form>
     </div>

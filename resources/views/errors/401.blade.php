48881600da413ff814e3f757b029b1d704e6aabe
Ayako Nakayama <j9griffon0220@gmail.com>
2025-12-29 19:45:01 +0900

    401ページ、文言とボタン追加、スタイリングカスタム調整

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -1,5 +1,34 @@
-@extends('errors::minimal')
+@extends('layouts.board_base')
+
+@section('board_content')
+  <div class="mx-auto mb-[var(--s4)] max-w-[15ch]">
+        <img
+            src="{{ asset('images/board-401.svg') }}"
+            alt="401エラー表示"
+            class="h-auto w-full"
+            />
+  </div>
+
+  <h1 class="text-double-blue font-title text-center mb-[var(--s6)] font-medium text-xl">
+    401 – ログイン認証が必要です
+  </h1>
+  <p class="mb-[var(--s5)] text-center font-body text-board-black font-normal text-base">
+    このページを表示するにはログインが必要です。
+    <br />
+    お手数ですが、ログインしてから再度お試しください。
+  </p>
+
+    {{-- ログイン画面へボタン --}}
+    <div class="flex justify-center mb-[var(--s4)]">
+        <div class="min-w-[15ch] block  text-center btn btn-secondary">
+            <a href="{{ route('login') }}">ログイン画面へ</a>
+        </div>
+    </div>
+@endsection
+
+
+{{-- @extends('errors::minimal')
 
 @section('title', __('Unauthorized'))
 @section('code', '401')
-@section('message', __('Unauthorized'))
+@section('message', __('Unauthorized')) --}}

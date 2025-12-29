5f3267d18a77efcde86386a4ece7744701d81c72
Ayako Nakayama <j9griffon0220@gmail.com>
2025-12-29 19:44:25 +0900

    404エラーページ、エラー画像をsvgに差し替え、条件分岐の戻るボタン追加

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,37 +1,39 @@
 @extends('layouts.board_base')
 
 @section('board_content')
-  <div class="mx-auto mt-[var(--s4)] max-w-[25vw]">
-    <picture>
-      <source
-        srcset="
-          {{ asset('images/error404@1x.webp') }} 1x,
-          {{ asset('images/error404@2x.webp') }} 2x
-        "
-        type="image/webp"
-      />
-      {{-- フォールバック --}}
-      <img src="{{ asset('images/error404.png') }}" alt="Trio Board Logo" class="h-auto w-full" />
-    </picture>
+  <div class="mx-auto mb-[var(--s4)] max-w-[15ch]">
+        <img
+            src="{{ asset('images/board-404.svg') }}"
+            alt="404エラー表示"
+            class="h-auto w-full"
+            />
   </div>
 
-  <h1 class="mt-[var(--s4)] text-center font-title text-double-blue">
+  <h1 class="text-double-blue font-title text-center mb-[var(--s6)] font-medium text-xl">
     404 – ページが見つかりません
   </h1>
-  <p class="mt-[var(--s4)] text-center font-body text-board-black">
+  <p class="mb-[var(--s5)] text-center font-body text-board-black font-normal text-base">
     URLが間違っているか、ページが移動した可能性があります。
     <br />
     お手数ですが、トップページから再度お探しください。
   </p>
 
-  <div class="text-center">
-    <a href="{{ route('login') }}" class="underline">ログインページへ</a>
-  </div>
+    {{-- スレッド一覧に戻るボタン --}}
+    <div class="flex justify-center mb-[var(--s4)]">
+        <div class="min-w-[15ch] block  text-center btn btn-secondary">
+            @if($isLoggedIn)
+            <a href="{{ route('threads.index') }}" class="font-title text-base">スレッド一覧に戻る</a>
+            @else
+            <a href="{{ route('login') }}">ログイン画面へ</a>
+            @endif
+        </div>
+    </div>
 @endsection
+
 
 {{--
   @extends('errors::minimal')
-  
+
   @section('title', __('Not Found'))
   @section('code', '404')
   @section('message', __('Not Found'))

fc2c7e894d61de8b3b5f345666e2319c66c61e21
Ayako Nakayama <j9griffon0220@gmail.com>
2025-12-29 19:39:12 +0900

    ポスト一覧、tailwindでスタイリング調整

--- resources/views/livewire/post-list.blade.php
+++ resources/views/livewire/post-list.blade.php
@@ -1,20 +1,28 @@
-<div>
+<div class="mb-[var(--s7)]">
   {{-- The best athlete wants his opponent at his best. --}}
-  <h1>これはlivewireのポスト一覧です</h1>
+  <h1 class="text-double-blue font-title text-center mb-[var(--s4)] font-medium text-xl">「{{ $thread->title }}」のポスト一覧</h1>
+
   @forelse ($this->posts as $post)
     {{-- postごとにまとめる --}}
-    <div>
-      <p>{{ optional($post->user)->name ?? '(投稿者不明)' }}</p>
-      <p>{{ $post->title }}</p>
-      <p>{{ $post->body }}</p>
-      <span
-        class="font-body text-board-charcoal"
-        title="{{ $thread->created_at->format('Y-m-d H:i') }}"
-      >
-        {{ $thread->created_at->diffForHumans() }}
-      </span>
+    <div class="max-w-[90vw] mb-[var(--s5)] p-[var(--s-2)] bg-board-blue/9">
+        <div class="flex gap-2 items-baseline mb-[var(--s-4)] max-w-[50ch]">
+            <p class="font-body text-board-black font-normal name-label ">
+                {{ optional($post->user)->name ?? '(投稿者不明)' }}</p>
+            <span
+            class="font-body text-board-charcoal font-light text-sm "
+            title="{{ $post->created_at->format('Y-m-d H:i') }}"
+            >
+            （{{ $post->created_at->diffForHumans() }}）
+            </span>
+        </div>
+        <div class="max-w-[50ch]">
+        <p class="font-body text-board-black font-normal mb-[var(--s-4)] ">
+            {{ $post->title }}</p>
+        <p class="font-body text-board-black font-normal">
+            {{ $post->body }}</p>
+        </div>
     </div>
   @empty
-    <p>投稿がまだありません</p>
+    <p class="font-body text-board-black font-normal mb-[var(--s-4)]">投稿がまだありません</p>
   @endforelse
 </div>

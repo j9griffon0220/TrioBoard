b1fa107c789d77d92a5e5cb8202f2e66f720ff0b
Ayako Nakayama <j9griffon0220@gmail.com>
2025-12-29 19:45:33 +0900

    403ページ、文言とボタン追加、スタイリングカスタム調整

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,5 +1,33 @@
-@extends('errors::minimal')
+@extends('layouts.board_base')
+
+@section('board_content')
+  <div class="mx-auto mb-[var(--s4)] max-w-[15ch]">
+        <img
+            src="{{ asset('images/board-403.svg') }}"
+            alt="403エラー表示"
+            class="h-auto w-full"
+            />
+  </div>
+
+  <h1 class="text-double-blue font-title text-center mb-[var(--s6)] font-medium text-xl">
+    403 – アクセス権がありません
+  </h1>
+  <p class="mb-[var(--s5)] text-center font-body text-board-black font-normal text-base">
+    ログインが必要なページ、またはアクセスが制限されているページの可能性があります。
+    <br />
+    お手数ですが、ログインしてから再度お試しください。
+  </p>
+
+    {{-- ログイン画面に戻るボタン --}}
+    <div class="flex justify-center mb-[var(--s4)]">
+        <div class="min-w-[15ch] block  text-center btn btn-secondary">
+            <a href="{{ route('login') }}">ログイン画面へ</a>
+        </div>
+    </div>
+@endsection
+
+{{-- @extends('errors::minimal')
 
 @section('title', __('Forbidden'))
 @section('code', '403')
-@section('message', __($exception->getMessage() ?: 'Forbidden'))
+@section('message', __($exception->getMessage() ?: 'Forbidden')) --}}

45bba0a535582fa07446ae51a4add7cf45c457c8
Ayako Nakayama <j9griffon0220@gmail.com>
2025-12-29 19:46:49 +0900

    500ページ、文言とボタン追加、スタイリングカスタム調整

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,5 +1,34 @@
-@extends('errors::minimal')
+@extends('layouts.board_base')
+
+@section('board_content')
+  <div class="mx-auto mb-[var(--s4)] max-w-[15ch]">
+        <img
+            src="{{ asset('images/board-500.svg') }}"
+            alt="500エラー表示"
+            class="h-auto w-full"
+            />
+  </div>
+
+  <h1 class="text-double-blue font-title text-center mb-[var(--s6)] font-medium text-xl">
+    500 – サーバーエラー
+  </h1>
+  <p class="mb-[var(--s5)] text-center font-body text-board-black font-normal text-base">
+    サーバーエラーが発生しました。ご不便をおかけして申し訳ありません。
+    <br />
+    復旧までしばらくお待ちいただければ幸いです。
+  </p>
+
+    {{-- トップページへ戻るボタン --}}
+    <div class="flex justify-center mb-[var(--s4)]">
+        <div class="min-w-[15ch] block  text-center btn btn-secondary">
+            <a href="{{ route('home') }}" class="font-title text-base">トップページへ戻る</a>
+        </div>
+    </div>
+@endsection
+
+
+{{-- @extends('errors::minimal')
 
 @section('title', __('Server Error'))
 @section('code', '500')
-@section('message', __('Server Error'))
+@section('message', __('Server Error')) --}}

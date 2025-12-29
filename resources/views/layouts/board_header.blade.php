93540f655da34fb48abbb0e3f58fe39ddae75ebc
Ayako Nakayama <j9griffon0220@gmail.com>
2025-12-29 19:35:10 +0900

    headerのロゴをsvgに差し替え調整

--- resources/views/layouts/board_header.blade.php
+++ resources/views/layouts/board_header.blade.php
@@ -1,13 +1,4 @@
-<div class="mx-auto max-w-[60vw] mb-[var(--s4)]">
-  <picture>
-    <source
-      srcset="
-        {{ asset('images/trio-board-logo@1x.webp') }} 1x,
-        {{ asset('images/trio-board-logo@2x.webp') }} 2x
-      "
-      type="image/webp"
-    />
-    {{-- フォールバック --}}
-    <img src="{{ asset('trio-board-logo.png') }}" alt="Trio Board Logo" class="h-auto w-full" />
-  </picture>
+<div class="mx-auto max-w-[60ch] mb-[var(--s6)]">
+    <img
+    src="{{ asset('images/trio-board-logo.svg') }}" alt="Trio Board Logo" class="h-auto w-full" />
 </div>

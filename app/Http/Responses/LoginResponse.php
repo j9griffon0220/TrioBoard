6fe1686c6a636882843c7ed62bbf01d4f61af2ee
Ayako Nakayama <j9griffon0220@gmail.com>
2026-01-06 08:54:06 +0900

    FortifyのLoginResponseに解説追加、ddコメントアウト

--- app/Http/Responses/LoginResponse.php
+++ app/Http/Responses/LoginResponse.php
@@ -6,15 +6,17 @@ use Laravel\Fortify\Contracts\LoginResponse as LoginResponseContract;
 use Illuminate\Http\Request;
 use App\Enums\Role;
 
+
 // viewerのみLoginするとDashboardに行かずにThread一覧画面に移動
+// Fortify用・viewer用制御
 // resources/views/livewire/auth/login.blade.php の方で制御するのでこちらは一旦見合わせ
 
 class LoginResponse implements LoginResponseContract
 {
     public function toResponse($request)
     {
-        dd($request->all())；
-        dd($user->role)；
+        // dd($request->all())；
+        // dd($user->role)；
         // dd('LoginResponse called');
         $user = $request->user();
         if($user->role === Role::Viewer){

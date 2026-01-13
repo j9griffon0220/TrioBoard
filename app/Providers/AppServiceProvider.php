13894b542e4058586edfe74a6ae6e2500d11584c
Ayako Nakayama <j9griffon0220@gmail.com>
2026-01-13 20:40:28 +0900

    AppServiceProviderを調整中

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,9 @@ use Illuminate\Support\ServiceProvider;
 use Laravel\Fortify\Contracts\LoginResponse as LoginResponseContract;
 use App\Actions\Fortify\LoginResponse;
 
+// use Filament\Http\Responses\Auth\Contracts\LoginResponse as FilamentLoginResponseContract;
+// use App\Http\Responses\FilamentLoginResponse;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -14,7 +17,10 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         // LoginResponseを登録（しないと適応されない）
-        // $this->app->singleton(LoginResponseContract::class, LoginResponse::class);
+        $this->app->singleton(LoginResponseContract::class, LoginResponse::class);
+        // $this->app->bind(
+        // FilamentLoginResponseContract::class,
+        // FilamentLoginResponse::class);
     }
 
     /**

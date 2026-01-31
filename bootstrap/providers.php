974fee0e164a82e68bc20559e63e6cb58582e573
Ayako Nakayama <j9griffon0220@gmail.com>
2026-01-31 19:02:23 +0900

    providerからfilament関連を削除

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,8 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
-    App\Providers\Filament\AdminPanelProvider::class,
-    App\Providers\Filament\MemberPanelProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
 ];

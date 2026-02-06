1c2f84d742e402488e42b0ee55b16a452118545e
Ayako Nakayama <j9griffon0220@gmail.com>
2026-02-06 20:57:48 +0900

    memberのmypageにスレッド一覧へのリンク追加

--- resources/views/livewire/member/mypage.blade.php
+++ resources/views/livewire/member/mypage.blade.php
@@ -1,5 +1,13 @@
 {{-- memberのmypage --}}
+<div>
 <h1>メンバーのマイページ画面</h1>
+
+<a href="{{ route('threads.index')}}"
+class="min-w-[15ch] inline-block text-center btn btn-secondary">
+    スレッド一覧へ
+</a>
+</div>
+
 {{--
   <div>
   {{-- Success is as dangerous as failure.

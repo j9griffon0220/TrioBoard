107b32e9de0908d6b6f7222788a81403fe04181a
Ayako Nakayama <j9griffon0220@gmail.com>
2026-02-26 20:57:24 +0900

    dashboard用レイアウトが適用されているかどうか、h1を入れて確認

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,5 +1,7 @@
 <x-layouts.app.header :title="$title ?? null">
-  <flux:main>
+    {{-- login後の管理画面台紙 --}}
+  <flux:main container>
+    <h1>Resources/vies/components/layouts/app.blade.phpが使われているか確認</h1>
     {{ $slot }}
   </flux:main>
 </x-layouts.app.header>

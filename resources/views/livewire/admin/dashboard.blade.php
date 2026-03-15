ab659f20fe01cb5d226e2d78fd7cb7ba85356d65
Ayako Nakayama <j9griffon0220@gmail.com>
2026-03-15 20:38:30 +0900

    管理画面のLivewire部分（右側メイン）のbladeファイルに記述を追加調整

--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -1,5 +1,42 @@
 {{-- 管理者adminのdashboard --}}
-<div>
+<div class="">
     {{-- 右メインサイド --}}
-    <p>admin画面です</p>
+
+    {{-- postごとにまとめる --}}
+    @forelse($posts as $post)
+        <div class="mb-[var(--s1)] p-[var(--s-2)] bg-board-blue/9 ">
+            <p class="font-body text-board-black font-normal mb-[var(--s-4)]">
+            <span class="font-body text-board-charcoal font-light text-sm name-label">
+            {{ $post->created_at->format('Y-m-d H:i') }}</span>
+            <span class="font-body text-board-charcoal font-light text-sm">
+            スレッド：</span>
+            {{ $post->thread->title ?? 'スレッド不明' }}
+            </p>
+
+            <p class="font-body text-board-black font-normal ">
+                <span class="font-body text-board-charcoal font-light text-sm">
+                投稿：</span>
+            {{ $post->body }}
+            </p>
+        </div>
+            @empty
+            {{-- 投稿がない時の表示 --}}
+            <p class="font-body text-board-black font-normal mb-[var(--s-4)]">
+            投稿がまだありません
+            </p>
+     @endforelse
+
+    {{-- @foreach($posts as $post)
+        <div class="mb-[var(--s1)] p-[var(--s-2)] bg-board-blue/9">
+        <p>
+        <span
+        class="font-body text-board-charcoal font-light text-sm ">
+        {{ $post->created_at->format('Y-m-d H:i') }}
+        </span>
+        スレッド： {{ $post->thread->title }}
+        <br>投稿：{{ $post->body }}
+        </p>
+        </div>
+    @endforeach --}}
+
 </div>

81584ec4b5f166c65fe402739e3ea20e48172342
Ayako Nakayama <j9griffon0220@gmail.com>
2026-03-15 20:41:59 +0900

    管理画面のLivewire部分のphpファイル、データ取得・描画の記述を追加

--- app/Livewire/Admin/Dashboard.php
+++ app/Livewire/Admin/Dashboard.php
@@ -3,11 +3,31 @@
 namespace App\Livewire\Admin;
 
 use Livewire\Component;
+use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
+// PostモデルとAuthを忘れずにインポート！！！
 
 class Dashboard extends Component
 {
     public function render()
     {
-        return view('livewire.admin.dashboard');
+        // 1.livewireでデータ取得・描画
+        $posts = Post::where('user_id', Auth::id())
+        ->with('thread')
+        ->latest()
+        ->paginate(10);
+
+        // 2.データをビューに渡す
+        return view('livewire.admin.dashboard',[
+            'posts' => $posts
+        ])
+        ->layout('components.layouts.admin-layout');
+
+        // return view('livewire.admin.dashboard',[
+        //     'posts' => Post::whrere('user_id', Auth::id()->get)
+        // ]);
+
+        // return view('livewire.admin.dashboard')
+        // ->layout('components.layouts.admin-layout');
     }
 }

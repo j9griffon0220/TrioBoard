c1fb6dcced34ed69c20b1dc22d9aec383574bcdb
Ayako Nakayama <j9griffon0220@gmail.com>
2026-03-15 20:41:08 +0900

    管理画面全体の台紙app.blade.phpに注釈コメントアウト等を追加

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,7 +1,9 @@
 <x-layouts.app.header :title="$title ?? null">
-    {{-- login後の管理画面台紙 --}}
-  <flux:main container>
-    <h1>Resources/vies/components/layouts/app.blade.phpが使われているか確認</h1>
+    {{-- login後の管理画面共通台紙 --}}
+    <flux:main {{ $attributes }}>
+    <h1 class="text-red-500">Resources/vies/components/layouts/app.blade.phpが使われているか確認</h1>
+    {{--  admin-layout または member-layout が入る --}}
+    {{-- 現状2カラムレイアウトは共通なのでadmin-layoutのみ --}}
     {{ $slot }}
-  </flux:main>
+    </flux:main>
 </x-layouts.app.header>

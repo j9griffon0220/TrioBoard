b3c2c86eb39eb87e4b18242491427abc63e77cf8
Ayako Nakayama <j9griffon0220@gmail.com>
2026-03-19 08:47:47 +0900

    dashboardのlayout、HTMLの構造とスタイリングを調整

--- resources/views/components/layouts/admin-layout.blade.php
+++ resources/views/components/layouts/admin-layout.blade.php
@@ -2,23 +2,22 @@
 <x-layouts.app title="Boardメンバーメニュー">
 
     <div class="max-w-[90]">
-    {{-- 左側リスト --}}
-    <div class="flex min-h-0 w-full flex-wrap">
-        <aside class="min-h-0 flex-1 flex-shrink-0 flex-grow basis-[30ch]">
-            <ul class="list-inside list-disc">
-                <li class="mb-[clamp(1.25rem,1.25rem+0.20vw,1.56rem)] max-w-8/10 px-4 py-2 text-[calc(0.85rem+0.2vw)] leading-[1.5] font-medium">
-                    マイポスト表示</li>
-                <li class="mb-[clamp(1.25rem,1.25rem+0.20vw,1.56rem)] max-w-8/10 px-4 py-2 text-[calc(0.85rem+0.2vw)] leading-[1.5] font-medium">
-                <a href="{{ route('threads.index')}}">
-                    スレッド一覧へ
-                </a>
-                </li>
-            </ul>
-        </aside>
-    </div>
+    {{-- メニュー・操作エリア --}}
+    <nav>
+        <ul class="flex w-full flex-wrap mb-[var(--s3)] items-center ">
+            <li class="max-w-[15ch] text-center p-(--s-5) max-w-8/10 text-[calc(0.85rem+0.2vw)] leading-[1.5] font-medium mr-[var(--s1)]">
+                ・マイポスト表示
+            </li>
+            {{-- 移動ボタン --}}
+            <li class="max-w-[15ch] btn btn-secondary inline-block list-none">
+                <a href="{{ route('threads.index') }}">スレッド一覧へ</a>
+            </li>
+        </ul>
+    </nav>
 
     {{-- 右側livewire画面 --}}
-    <div class="min-h-0 flex-1 flex-shrink-0 flex-grow basis-[30ch]">
+    {{-- <div class="min-h-0 flex-1 flex-shrink-0 flex-grow basis-[30ch]"> --}}
+    <div>
         {{$slot}}
     </div>
 

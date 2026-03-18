446e5966ce28ae3b656f0ec17ab1a6f23bbf1342
Ayako Nakayama <j9griffon0220@gmail.com>
2026-03-19 08:49:43 +0900

    ポスト一覧画面、tailwindのスタイリング追加調整

--- resources/views/livewire/post-list.blade.php
+++ resources/views/livewire/post-list.blade.php
@@ -1,12 +1,12 @@
 <div class="mb-[var(--s7)]">
   {{-- The best athlete wants his opponent at his best. --}}
-  <h1 class="text-double-blue font-title text-center mb-[var(--s4)] font-medium text-xl">「{{ $thread->title }}」のポスト一覧</h1>
+  <h1 class="text-double-blue font-title text-center mb-[var(--s4)] font-medium text-2xl">「{{ $thread->title }}」のポスト一覧</h1>
 
   @forelse ($this->posts as $post)
     {{-- postごとにまとめる --}}
-    <div class="max-w-[90vw] mb-[var(--s5)] p-[var(--s-2)] bg-board-blue/9">
-        <div class="flex gap-2 items-baseline mb-[var(--s-4)] max-w-[50ch]">
-            <p class="font-body text-board-black font-normal name-label ">
+    <div class="max-w-[90vw] mb-[var(--s5)] p-[var(--s-1)] bg-board-blue/9">
+        <div class="flex gap-2 items-baseline mb-[var(--s-4)] max-w-[50ch] ">
+            <p class="font-body text-board-black font-normal name-label text-sm">
                 {{ optional($post->user)->name ?? '(投稿者不明)' }}</p>
             <span
             class="font-body text-board-charcoal font-light text-sm "
@@ -16,13 +16,13 @@
             </span>
         </div>
         <div class="max-w-[50ch]">
-        <p class="font-body text-board-black font-normal mb-[var(--s-4)] ">
-            {{ $post->title }}</p>
-        <p class="font-body text-board-black font-normal">
-            {{ $post->body }}</p>
+            <p class="font-body text-board-black font-normal mb-[var(--s-4)] text-base">
+                {{ $post->title }}</p>
+            <p class="font-body text-board-black font-normal text-base">
+                {{ $post->body }}</p>
         </div>
     </div>
   @empty
-    <p class="font-body text-board-black font-normal mb-[var(--s-4)]">投稿がまだありません</p>
+    <p class="font-body text-board-black font-normal mb-[var(--s-4)] text-base">投稿がまだありません</p>
   @endforelse
 </div>

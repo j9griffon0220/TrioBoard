7e4874e06d28744220092956faae3293449d3646
Ayako Nakayama <j9griffon0220@gmail.com>
2026-03-29 18:40:55 +0900

    memberのmypage、Livewireコンポーネントのロジックを記述

--- app/Livewire/Member/Mypage.php
+++ app/Livewire/Member/Mypage.php
@@ -3,11 +3,25 @@
 namespace App\Livewire\Member;
 
 use Livewire\Component;
+use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
+// PostモデルとAuthを忘れずにインポート！！！
 
 class Mypage extends Component
 {
-    public function render()
-    {
-        return view('livewire.member.mypage');
+    public function render(){
+        // 1.livewireでデータ取得・描画
+        $posts = Post::where('user_id', Auth::id())
+        ->with('thread')
+        ->latest()
+        ->paginate(10);
+
+        // 2.データをビューに渡す
+        return view('livewire.member.mypage',[
+            'posts' => $posts
+        ])
+        // ->layoutでLivewire のレイアウト指定
+        // adminと同じlayoutのbladeファイルを使用している
+        ->layout('components.layouts.admin-layout');
     }
 }

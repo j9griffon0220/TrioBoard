e2ac5c7e1ab3820d25bb3fd2c503af26904cd706
Ayako Nakayama <j9griffon0220@gmail.com>
2026-04-03 08:19:54 +0900

    Prettierによる自動整形

--- resources/views/Threads/index.blade.php
+++ resources/views/Threads/index.blade.php
@@ -1,55 +1,61 @@
 @extends('layouts.board_base')
 @section('board_content')
-
-<h1 class="text-double-blue font-title text-center mb-[var(--s4)] font-medium text-xl">スレッド一覧</h1>
-@can('create',\App\Models\Thread::class)
-<div class="flex justify-end">
-    <div class="min-w-[15ch] text-center mb-[var(--s4)]">
-    <a href="{{ route('threads.create') }}"
-    class="block font-title text-center text-base btn btn-primary">スレッド作成</a>
+  <h1 class="mb-[var(--s4)] text-center font-title text-xl font-medium text-double-blue">
+    スレッド一覧
+  </h1>
+  @can('create', \App\Models\Thread::class)
+    <div class="flex justify-end">
+      <div class="mb-[var(--s4)] min-w-[15ch] text-center">
+        <a
+          href="{{ route('threads.create') }}"
+          class="block btn-primary btn text-center font-title text-base"
+        >
+          スレッド作成
+        </a>
+      </div>
     </div>
-</div>
-@endcan
+  @endcan
 
-@if(session('success'))
+  @if (session('success'))
     <div>
-        <p class="text-double-red font-body font-normal">{{ session('success') }}</p>
+      <p class="font-body font-normal text-double-red">{{ session('success') }}</p>
     </div>
-@endif
+  @endif
 
-{{-- スレッド表示 --}}
-<ul>
+  {{-- スレッド表示 --}}
+  <ul>
     @forelse ($threads as $thread)
-        <li class="text-board-black font-body font-normal text-base border-1 p-[var(--s-3)] border-board-border mb-[var(--s4)] max-w-[80vw] border-l-5 border-l-board-blue
-        hover:shadow-xl transition-shadow bg-gray-50">
-            <a href="{{ route('threads.show', $thread->id )}}">
-                {{ $thread->title }}
-            <span class="font-body text-board-charcoal font-light text-sm" title="{{ $thread->created_at->format('Y-m-d H:i')}}">
-                （{{ $thread->created_at->diffForHumans() }}）
-            </span>
-            </a>
-        </li>
+      <li
+        class="font-body mb-[var(--s4)] max-w-[80vw] border-1 border-l-5 border-board-border border-l-board-blue bg-gray-50 p-[var(--s-3)] text-base font-normal text-board-black transition-shadow hover:shadow-xl"
+      >
+        <a href="{{ route('threads.show', $thread->id) }}">
+          {{ $thread->title }}
+          <span
+            class="font-body text-sm font-light text-board-charcoal"
+            title="{{ $thread->created_at->format('Y-m-d H:i') }}"
+          >
+            （{{ $thread->created_at->diffForHumans() }}）
+          </span>
+        </a>
+      </li>
     @empty
-    <p class="text-board-black font-body font-normal text-base">まだスレッドがありません</p>
+      <p class="font-body text-base font-normal text-board-black">まだスレッドがありません</p>
     @endforelse
-</ul>
+  </ul>
 
-<div class="flex justify-end mb-[var(--s4)]">
+  {{-- ページネーションリンクの表示 --}}
+  <div class="mb-[var(--s4)]">
+    {{ $threads->links() }}
+  </div>
+
+  <div class="mb-[var(--s4)] flex justify-end">
     <div class="max-w-[15ch]">
-        <form method="POST" action="{{ route('logout') }}">
+      <form method="POST" action="{{ route('logout') }}">
         @csrf
-        <button type="submit" class="btn btn-outline">
-            <img
-              src="{{ asset('images/board-logout.svg') }}"
-              alt="logout"
-              class="h-auto w-full"
-            />
+        <button type="submit" class="btn-outline btn">
+          <img src="{{ asset('images/board-logout.svg') }}" alt="logout" class="h-auto w-full" />
         </button>
-        </form>
+      </form>
     </div>
-</div>
-
+  </div>
 @endsection
-
-
-

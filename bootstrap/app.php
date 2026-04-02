fa1198a8f8d05a60ea2c1d0b68d7ca4434110b2f
Ayako Nakayama <j9griffon0220@gmail.com>
2026-04-03 08:39:52 +0900

    デフォルトのdashboardへの遷移を止めるため、app.phpにroleごとの処理を追加

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+use Illuminate\Auth\Middleware\RedirectIfAuthenticated;
+use App\Enums\Role;
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -16,6 +19,18 @@ return Application::configure(basePath: dirname(__DIR__))
             'is_admin' => \App\Http\Middleware\IsAdmin::class,
             'is_member' => \App\Http\middleware\IsMember::class,
         ]);
+
+            RedirectIfAuthenticated::redirectUsing(function () {
+        $user = Auth::user();
+
+        return match ($user->role) {
+            \App\Enums\Role::Admin => route('admin.dashboard'),
+            \App\Enums\Role::Member => route('member.mypage'),
+            \App\Enums\Role::Viewer => route('threads.index'),
+            default => route('login'),
+        };
+    });
+
     })
     // ->withProviders([
     // App\Providers\FortifyServiceProvider::class, // ←★ これを追加

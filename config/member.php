35ab921f21311259508d9a32354a6ac191289140
Ayako Nakayama <j9griffon0220@gmail.com>
2026-04-11 18:11:03 +0900

    デプロイ後のリアルmember用configファイル作成

---
+++ config/member.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /* メンバー用設定・複数いるので配列でまとめる*/
+    'members' => [
+        'member1' => [
+            'name' => env('MEMBER1_NAME'),
+            'email' => env('MEMBER1__EMAIL'),
+            'password' => env('MEMBER1_PASSWORD'),
+        ],
+        'member2' => [
+            'name' => env('MEMBER2_NAME'),
+            'email' => env('MEMBER2__EMAIL'),
+            'password' => env('MEMBER2_PASSWORD'),
+        ],
+    ],
+];
